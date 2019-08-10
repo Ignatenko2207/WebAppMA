45cb5e0482158e0f19892f7575139460f344b55d
alexo <alexogrebchuk@gmail.com>
2019-08-10 03:48:20 +0300

    optimize imports

--- src/main/java/com/mainacad/model/Cart.java
+++ src/main/java/com/mainacad/model/Cart.java
@@ -1,6 +1,9 @@
 package com.mainacad.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Setter
 @Getter
